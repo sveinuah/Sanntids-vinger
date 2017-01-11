8606ee10b5a5b4bfe9df66e4ef7f25473228723f
Sveinung Haugane <sveihaug@gmail.com>
2017-01-11 19:29:35 +0100

    Added Two hello world test files in the go language

---
+++ helloworld_go.go
@@ -0,0 +1,25 @@
+// Go 1.2
+// go run helloworld_go.go
+
+package main
+
+import (
+    . "fmt"
+    "runtime"
+    "time"
+)
+
+func someGoroutine() {
+    Println("Hello from a goroutine!")
+}
+
+func main() {
+    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
+                                            // Try doing the exercise both with and without it!
+    go someGoroutine()                      // This spawns someGoroutine() as a goroutine
+
+    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
+    // We'll come back to using channels in Exercise 2. For now: Sleep.
+    time.Sleep(100*time.Millisecond)
+    Println("Hello from main!")
+}
